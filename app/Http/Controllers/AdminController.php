8f789e9ce5364dde9016269fba65fb85a07ea342
Mohamad Gamar <gamarbaik69@gmail.com>
2025-11-23 22:49:35 +0700

    Fix view path case sensitivity in AdminController

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -174,7 +174,7 @@ class AdminController extends Controller
         elseif ($user->role === 'siswa' && $user->siswa) $currentName = $user->siswa->namaSiswa;
         elseif ($user->role === 'pembina' && $user->pembina) $currentName = $user->pembina->nama;
 
-        return view('Admin.users-edit', compact('user', 'currentName', 'kelas'));
+        return view('admin.users-edit', compact('user', 'currentName', 'kelas'));
     }
 
     public function updateUser(Request $request, $id)
